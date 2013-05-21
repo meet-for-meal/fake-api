62765a08e28e3024f9702741290997aa0735b490
rhannequin <remy.hannequin@gmail.com>
2013-05-21 21:55:51 +0200

    Update users

--- routes/user.js
+++ routes/user.js
@@ -8,24 +8,54 @@ exports.list = function(req, res){
     id: 1,
     firstname: 'Rémy',
     lastname: 'Funky',
-    username: 'rhannequin',
-    created_at: new Date(2013, 5, 11, 12, 0, 0)
+    age: 22,
+    email: 'remy@gmail.com',
+    password: 'remy',
+    gender: 1,
+    firstVisit: 1,
+    lastPosition: new Date(2013, 2, 27, 12, 15, 0),
+    lastLatitude: 48.8787394,
+    lastLongitude: 2.4
   }, {
     id: 2,
     firstname: 'Hervé',
     lastname: 'Jagbomb',
-    username: 'jagbomb',
-    created_at: new Date(2013, 5, 11, 14, 30, 0)
+    age: 22,
+    email: 'herve@gmail.com',
+    password: 'herve',
+    gender: 1,
+    firstVisit: 1,
+    lastPosition: new Date(2013, 2, 27, 12, 15, 0),
+    lastLatitude: 48.8787394,
+    lastLongitude: 2.4
+  }, {
+    id: 3,
+    firstname: 'Julien',
+    lastname: 'WETSTEIN',
+    age: 24,
+    email: 'julienwetstein@gmail.com',
+    password: 'thunder',
+    gender: 1,
+    firstVisit: 1,
+    lastPosition: new Date(2013, 2, 27, 12, 15, 0),
+    lastLatitude: 48.8787394,
+    lastLongitude: 2.4
   }]);
 };
 
 exports.show = function(req, res){
   res.jsonp({
-    id: 1,
-    firstname: 'Rémy',
-    lastname: 'Funky',
-    username: 'rhannequin',
-    created_at: new Date(2013, 5, 11, 12, 0, 0)
+    id: 3,
+    firstname: 'Julien',
+    lastname: 'WETSTEIN',
+    age: 24,
+    email: 'julienwetstein@gmail.com',
+    password: 'thunder',
+    gender: 1,
+    firstVisit: 1,
+    lastPosition: new Date(2013, 2, 27, 12, 15, 0),
+    lastLatitude: 48.8787394,
+    lastLongitude: 2.4
   });
 };
 
